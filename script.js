7e3f0ef9c743a8421e7523d5ba23811b789a53e2
Justin Ramirez <ramirezbjustin@gmail.com>
2021-03-25 18:25:09 -0400

    debugging rotational icons

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ document.querySelector('.menu').addEventListener('click', () => {
 });
 
 const icons = document.querySelectorAll('.section-1-icons i');
-
 let i = 1;
 
 setInterval(() => {
   i++;
-
-  const icon = document.querySelector('.section-1-icons.change');
-
+  const icon = document.querySelector('.section-1-icons .change');
   icon.classList.remove('change');
 
   if (i > icons.length) {
